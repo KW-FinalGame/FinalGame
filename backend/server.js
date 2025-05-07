46bac6c4715e3576722c530df33d8e7036b1cb8b
HeeJi <nitej394@naver.com>
2025-05-07 11:24:23 +0900

    라우터 삭제

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,6 @@ const server = http.createServer(app); // http 서버 생성
 const authRoutes = require('./routes/authRoutes'); // 라우터 파일 가져오기
 const signRoutes = require('./routes/signRoutes');
 const mapRoutes = require('./routes/mapRoutes');
-const subwayRoutes = require('./routes/subwayRoutes');
 const socketHandler = require('./modules/socketHandler');
 
 
@@ -49,7 +48,6 @@ const authenticateToken = (req, res, next) => {
 app.use('/', authRoutes); // '/' 경로로 authRoutes를 설정
 app.use('/', signRoutes); // roomRoutes 추가
 app.use('/', mapRoutes);
-app.use('/', subwayRoutes);
 
 // 배포 시 활성화 할 부분
 // // React 정적 파일 제공
